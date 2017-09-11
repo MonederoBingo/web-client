0f93f88295e72d7db5448eb4750ac62e7ff9cb04
alayor <alayor3@gmail.com>
2017-09-10 21:15:57 -0500

    Adding test to check test service is not called if there is an existing xsrf token.

--- __tests__/src/api/apiUtils.spec.js
+++ __tests__/src/api/apiUtils.spec.js
@@ -18,7 +18,7 @@ describe("api utils", () => {
         //then
         expect(typeof result.then === "function").toBe(true);
     });
-    it("should call service to get xsrf token if document.cookie is null", () => {
+    it("should call test service to get xsrf token if document.cookie is null", () => {
         //given
         document.cookie = null;
 
@@ -26,7 +26,29 @@ describe("api utils", () => {
         let result = apiUtils.callApiService();
 
         //then
-        expect(fetch).toBeCalled();
+        expect(fetch).toBeCalledWith('http://localhost:9090/test', {credentials: 'include'});
+    });
+    it("should call test service to get xsrf token if document has no XSRF-TOKEN cookie", () => {
+        //given
+        document.cookie = "JSESSIONID=1234;";
+
+        //when
+        let result = apiUtils.callApiService();
+
+        //then
+        expect(fetch).toBeCalledWith('http://localhost:9090/test', {credentials: 'include'});
+    });
+    it("should not call test service to get xsrf token if document has XSRF-TOKEN cookie", () => {
+        //given
+        fetch.mockClear();
+        document.cookie = "XSRF-TOKEN=1234;";
+
+        //when
+        let result = apiUtils.callApiService();
+
+        //then
+        expect(fetch).not.toBeCalledWith('http://localhost:9090/test', {credentials: 'include'});
+
     });
   });
 });
