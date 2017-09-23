59cc56c94bc382ec826d243270dd6f2506d3da5f
alayor <alayor3@gmail.com>
2017-09-22 22:35:38 -0500

    adding unit test with snapshot comparison

--- src/components/pointsAwarding/__tests__/children/PointsAwardingForm.spec.js
+++ src/components/pointsAwarding/__tests__/children/PointsAwardingForm.spec.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import expect from 'expect';
 import PointsAwardingForm from '../../children/PointsAwardingForm';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
 import NumberInput from "../../../common/NumberInput";
-const renderer = new ReactShallowRenderer();
 
 describe("PointsAwardingForm", () => {
   it("should show initial layout", () => {
      // when
-     renderer.render(<PointsAwardingForm />);
+     const wrapper = shallow(<PointsAwardingForm />);
 
      // then
-     const result = renderer.getRenderOutput();
-     expect(result.type).toBe('div');
-     expect(result.props.children).toEqual([
-        <NumberInput name='phoneNumber' label='Phone #' onChange={() => {}} placeholder='Phone #'/>,
-        <NumberInput name='amount' label='Amount' onChange={() => {}} placeholder='Amount'/>
-     ]);
+     expect(wrapper.find('div').children().nodes).toMatchSnapshot();
   });
 });
