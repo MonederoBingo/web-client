e763322417c847c444669ddbf31499f010ef79e6
alayor <alayor3@gmail.com>
2017-09-23 11:16:53 -0500

    adding text and button to points awarding form

--- src/components/pointsAwarding/children/PointsAwardingForm.js
+++ src/components/pointsAwarding/children/PointsAwardingForm.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import NumberInput from "../../common/NumberInput";
+import TextInput from "../../common/TextInput";
 
 const PointsAwarding = (props) => {
   return (
      <div>
        <NumberInput name='phoneNumber' label='Phone #' onChange={() => {}} placeholder='Phone #'/>
        <NumberInput name='amount' label='Amount' onChange={() => {}} placeholder='Amount'/>
+       <TextInput name='saleKey' label="Sale Key" onChange={() => {}} placeholder=""/>
+       <input id="save" type="submit" disabled={() => {}} value="Save" className="btn btn-primary" onClick={() => {}} />
      </div>
   );
 };
