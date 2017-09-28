060541b41af655cb76af9e090c371120ff911f90
alayor <alayor3@gmail.com>
2017-09-27 22:27:04 -0500

    Add test to chech that component methods are called in form

--- src/components/pointsAwarding/PointsAwardingPage.js
+++ src/components/pointsAwarding/PointsAwardingPage.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import PointsAwardingForm from './children/PointsAwardingForm';
 
-const PointsAwardingPage = (props) => {
-  return (
-     <div>
-       <PointsAwardingForm />
-     </div>
-  );
-};
+class PointsAwardingPage extends React.Component {
+  change() {
+
+  }
+   render() {
+     return (
+       <div>
+         <PointsAwardingForm
+           onChange={this.change}
+         />
+       </div>
+     );
+   }
+}
 
 export default PointsAwardingPage;
--- src/components/pointsAwarding/__tests__/PointsAwardingPage.spec.js
+++ src/components/pointsAwarding/__tests__/PointsAwardingPage.spec.js
@@ -6,10 +6,21 @@ import { shallow } from 'enzyme';
 
 describe("PointsAwardingPage", () => {
   it("should render initial layout", () => {
-     // when
-     const wrapper = shallow(<PointsAwardingPage />);
+    // when
+    const wrapper = shallow(<PointsAwardingPage/>);
 
-     // then
-     expect(wrapper.find('div').children().nodes).toMatchSnapshot();
+    // then
+    expect(wrapper.find('div').children().nodes).toMatchSnapshot();
+  });
+  it("should call change method in form", () => {
+    // given
+    spyOn(PointsAwardingPage.prototype, 'change').and.callThrough();
+    const form = shallow(<PointsAwardingPage />).find('PointsAwardingForm');
+
+    // when
+    form.props().onChange();
+
+    // then
+    expect(PointsAwardingPage.prototype.change).toHaveBeenCalled();
   });
 });
