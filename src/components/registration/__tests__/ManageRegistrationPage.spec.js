fda209ddfa1071f124e9f7c588ba23409ff1f350
alayor <alayor3@gmail.com>
2017-10-08 17:28:42 -0500

    Finishing tests in registration page

--- src/components/registration/__tests__/ManageRegistrationPage.spec.js
+++ src/components/registration/__tests__/ManageRegistrationPage.spec.js
@@ -68,67 +68,43 @@ describe("ManageRegistrationPage", () => {
     // then
     expect(spy).toHaveBeenCalled();
   });
-  // it("should pass pointsAwarding object from state to form", () => {
-  //   // when
-  //   component.setState({pointsAwarding: {}});
-  //
-  //   // then
-  //   expect(component.find('RegistrationForm').props().pointsAwarding).toEqual({});
-  // });
-  // it("should update phone number in state", () => {
-  //   // given
-  //   const form = component.find('RegistrationForm');
-  //   const event = withEvent("phoneNumber", "345");
-  //
-  //   // when
-  //   form.props().onChange(event);
-  //
-  //   // then
-  //   expect(component.instance().state.pointsAwarding.phoneNumber).toEqual("345");
-  // });
-  // it("should update amount in state", () => {
-  //   // given
-  //   const form = component.find('RegistrationForm');
-  //   const event = withEvent("amount", 100);
-  //
-  //   // when
-  //   form.props().onChange(event);
-  //
-  //   // then
-  //   expect(component.instance().state.pointsAwarding.amount).toEqual(100);
-  // });
-  // it("should update sale key in state", () => {
-  //   // given
-  //   const form = component.find('RegistrationForm');
-  //   const event = withEvent("saleKey", "key");
-  //
-  //   // when
-  //   form.props().onChange(event);
-  //
-  //   // then
-  //   expect(component.instance().state.pointsAwarding.saleKey).toEqual("key");
-  // });
-  // it("should map award points actions to props", () => {
-  //   // given
-  //   const connectedComponent = mount( <Provider store={mockStore()}><ConnectedManageRegistrationPage /></Provider> );
-  //
-  //   // when
-  //   const component = connectedComponent.find('ManageRegistrationPage');
-  //
-  //   // then
-  //   expect(component.props().actions.awardPoints).toBeInstanceOf(Function);
-  // });
-  // it("should call award points action on submit", () => {
-  //   // given
-  //   const connectedComponent = mount( <Provider store={mockStore()}><ConnectedManageRegistrationPage /></Provider> );
-  //   const component = connectedComponent.find('ManageRegistrationPage');
-  //   const form = component.find('RegistrationForm');
-  //   component.props().actions.awardPoints = jest.fn();
-  //
-  //   // when
-  //   form.props().onSubmit();
-  //
-  //   // then
-  //   expect(component.props().actions.awardPoints).toHaveBeenCalled();
-  // });
+  it("should pass user object from state to form", () => {
+    // when
+    component.setState({user: {username: "username1"}});
+
+    // then
+    expect(component.find('RegistrationForm').props().user).toEqual({username: "username1"});
+  });
+  it("should update phone number in state", () => {
+    // given
+    const form = component.find('RegistrationForm');
+    const event = withEvent("username", "newUsername");
+
+    // when
+    form.props().onChange(event);
+
+    // then
+    expect(component.instance().state.user.username).toEqual("newUsername");
+  });
+  it("should update password in state", () => {
+    // given
+    const form = component.find('RegistrationForm');
+    const event = withEvent("password", "password");
+
+    // when
+    form.props().onChange(event);
+
+    // then
+    expect(component.instance().state.user.password).toEqual("password");
+  });
+  it("should map registration actions to props", () => {
+    // given
+    const connectedComponent = mount( <Provider store={mockStore()}><ConnectedManageRegistrationPage /></Provider> );
+
+    // when
+    const component = connectedComponent.find('ManageRegistrationPage');
+
+    // then
+    expect(component.props().actions.createUser).toBeInstanceOf(Function);
+  });
 });
