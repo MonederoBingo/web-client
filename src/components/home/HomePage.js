b65e27cd4e754198a77c4425798d560471aea1de
alayor <alayor3@gmail.com>
2017-10-07 20:14:32 -0500

    Add script to start services

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -5,8 +5,8 @@ class HomePage extends React.Component {
   render() {
     return (
       <div className="jumbotron">
-        <h1>Great App XYZ</h1>
-        <h2>Jogging Times App</h2>
+        <h1>Monedero Bingo</h1>
+        <h2>Promotions and giveaways</h2>
         <br />
         <Link to="register" className="btn btn-primary btn-lg">Register</Link>
         &nbsp;
